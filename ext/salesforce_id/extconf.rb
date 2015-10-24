ce2a723d094771b24159f55014cfd7f196fadd11
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2015-10-24 04:24:35 +0200

    Improved build process to use c99

--- ext/salesforce_id/extconf.rb
+++ ext/salesforce_id/extconf.rb
@@ -1,3 +1,6 @@
 require "mkmf"
 
-create_makefile("salesforce_id/salesforce_id")
+# CFLAGS   = $(CCDLFLAGS) $(cflags)  -fno-common -pipe $(ARCH_FLAG)
+with_cflags("$(cflags) -std=c99 -fno-common -pipe") do
+  create_makefile("salesforce_id/salesforce_id")
+end
