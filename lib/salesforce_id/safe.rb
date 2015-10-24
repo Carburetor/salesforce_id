d174ebc27c0ab045f6770c0721596dfc687d1a93
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2015-10-24 06:14:26 +0200

    Added safe class to handle id easily

---
+++ lib/salesforce_id/safe.rb
@@ -0,0 +1,49 @@
+require 'salesforce_id'
+
+module SalesforceId
+
+  # Rapresents a salesforce id as an object allowing for comparison and usage
+  # in a safe fashion thanks to using fixed format. Internally will store and
+  # keep any salesforce id using the case-insensitive format but with repaired
+  # casing, which should offer maximum safety
+  class Safe
+    include Comparable
+
+    # @param id [String] valid salesforce id
+    def initialize(id)
+      unless ::SalesforceId.valid?(id)
+        raise ArgumentError, "Salesforce ID not valid"
+      end
+
+      @value = SalesforceId.to_insensitive(id.dup).freeze
+    end
+
+    # @return [String] text composing salesforce id, string is **frozen**
+    def to_s
+      value
+    end
+
+    def <=>(other)
+      value <=> other.value
+    end
+
+    # @return [String] salesforce id in case-sensitive format, string is **not**
+    # frozen
+    def to_sensitive
+      SalesforceId.to_sensitive(value.dup)
+    end
+
+    # @return [String] salesforce id in case-insensitive format, string is
+    # **not** frozen
+    def to_insensitive
+      to_s.dup
+    end
+
+    protected
+
+    # Internal value of salesforce safe id
+    attr_reader :value
+
+  end
+
+end
