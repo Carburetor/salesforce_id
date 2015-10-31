dc931d89384d0d50e401d2a48604bd171aaa07a5
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2015-10-31 17:23:17 +0100

    Added tests for coverage and refactored code to check for injection on visitor

--- lib/salesforce_id/arel.rb
+++ lib/salesforce_id/arel.rb
@@ -1,12 +1,6 @@
 require 'rubygems'
+require 'salesforce_id/arel/visitor'
 
-arel_is_present   = defined?(::Arel)
-arel_is_present &&= Gem::Dependency.new('arel', '~> 5.0')
-                                   .match?('arel', ::Arel::VERSION)
-
-if arel_is_present
-  require 'arel'
-  require 'salesforce_id/arel/visitor'
-
+if ::SalesforceId::Arel::Visitor.injectable?
   ::Arel::Visitors::ToSql.send(:include, ::SalesforceId::Arel::Visitor)
 end
